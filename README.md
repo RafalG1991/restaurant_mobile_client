1b72c51ee17fd7212ae3aa0c771efc86fd7704dc
Rafal <rgrajcar@gmail.com>
2025-11-17 19:44:29 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# restaurant_client
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/restaurant_client/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.restaurant_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()

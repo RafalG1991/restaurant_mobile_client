35156a5f496e99c3eacdbf80951db96f84fc7aab
Rafal <rgrajcar@gmail.com>
2025-11-17 19:57:13 +0100

    added main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'state/order_provider.dart';
+import 'ui/scan_page.dart';
+import 'ui/menu_page.dart';
+import 'ui/summary_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => OrderProvider()..hydrate()),
+      ],
+      child: const PubClientApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class PubClientApp extends StatelessWidget {
+  const PubClientApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Pub Client',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        useMaterial3: true,
       ),
+      routes: {
+        '/': (_) => const ScanPage(),
+        '/menu': (_) => const MenuPage(),
+        '/summary': (_) => const SummaryPage(),
+      },
     );
   }
 }

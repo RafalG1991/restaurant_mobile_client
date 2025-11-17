be11377fc5ec111e09355fcf2ecdbb37737a265c
Rafal <rgrajcar@gmail.com>
2025-11-17 19:57:51 +0100

    added BasketItem class

---
+++ lib/models/order.dart
@@ -0,0 +1,8 @@
+class BasketItem {
+  final int id;
+  final String name;
+  final double price;
+  int quantity;
+
+  BasketItem({required this.id, required this.name, required this.price, required this.quantity});
+}
